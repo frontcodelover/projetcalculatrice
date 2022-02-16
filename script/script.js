aa4bcef2e7caa4ee652445dd3291f133f82af2d8
frontcodelover <contact@avantjetaisriche.com>
2022-02-16 17:18:54 +0100

    poursuite du projet

---
+++ script/script.js
@@ -0,0 +1,43 @@
+let element = document.getElementById('output')
+
+
+function zero() {    
+    if(element.innerHTML === "0") {
+        element.innerHTML = ""
+    }
+}
+
+// affiche les nombres
+function getValue(myValue) {
+    zero();
+    element.innerHTML += myValue
+  }
+
+function resultat() {
+    zero()
+     var equation = document.getElementById("output").innerHTML;
+     var solved = eval(equation);
+     document.getElementById('output').innerHTML = solved;
+}
+
+function clearCalc() {
+    document.getElementById("output").innerHTML = "0";
+}
+
+function magie() {
+    const magie = document.querySelector(".calBody");
+    magie.style.transform = "rotate(180deg)";
+    magie.style.transition = "transform .8s ease-in-out"
+}
+
+
+  // affichage curseur en rond
+function cercle() {
+    const cercle = document.querySelector('.cercle');        
+    document.addEventListener('mousemove', (e) => {
+        cercle.style.left = `${e.pageX}px`;
+        cercle.style.top = `${e.pageY}px`;
+    })
+}
+
+cercle()
